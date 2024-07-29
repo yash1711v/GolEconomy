60f4bb33c0d124befd719d4e70779f762330146d
Yash verma <yv48183@gmail.com>
2024-07-29 15:16:05 +0530

    refactor: integration testing also added

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import '/Volumes/Yashverma/Flutter/FlutterProjects/gol_economy/lib/main.dart' as app;
 void main() => run(_testMain);
@@ -5,7 +6,9 @@ void main() => run(_testMain);
 run(void Function() testMain) {}
 
 void _testMain() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('end to end testing ', (tester) async {
     app.mainCommon();
+    await tester.pumpAndSettle();
+    await tester.allWidgets;
   });
 }
